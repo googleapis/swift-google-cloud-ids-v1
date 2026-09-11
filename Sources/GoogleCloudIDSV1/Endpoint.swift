b8dd45e03c790da2d957fdc6a6f40252692a288e
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudIDSV1/Endpoint.swift
+++ Sources/GoogleCloudIDSV1/Endpoint.swift
@@ -186,12 +186,12 @@ public struct Endpoint: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .informational: return try container.encode(1)
-      case .low: return try container.encode(2)
-      case .medium: return try container.encode(3)
-      case .high: return try container.encode(4)
-      case .critical: return try container.encode(5)
+      case .unspecified: return try container.encode("SEVERITY_UNSPECIFIED")
+      case .informational: return try container.encode("INFORMATIONAL")
+      case .low: return try container.encode("LOW")
+      case .medium: return try container.encode("MEDIUM")
+      case .high: return try container.encode("HIGH")
+      case .critical: return try container.encode("CRITICAL")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
@@ -300,10 +300,10 @@ public struct Endpoint: Codable, Equatable, GoogleCloudWKT._AnyPackable,
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .creating: return try container.encode(1)
-      case .ready: return try container.encode(2)
-      case .deleting: return try container.encode(3)
+      case .unspecified: return try container.encode("STATE_UNSPECIFIED")
+      case .creating: return try container.encode("CREATING")
+      case .ready: return try container.encode("READY")
+      case .deleting: return try container.encode("DELETING")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
